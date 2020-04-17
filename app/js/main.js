99838bfe3357bd5a2c4b348623171b1f6a7ea28a
farmProw <188hardbass@gmail.com>
2020-04-17 19:01:16 +0300

    add

--- app/js/main.js
+++ app/js/main.js
@@ -26,11 +26,18 @@ $(function(){
      $('.icon-th-list').removeClass('active')
 
    });
-     $(".product-page__items .product-slider__inner").slick({
+     $(".product-slider__inner").slick({
        dots:true,
        arrows:false,
        slidesToShow: 4,
        slidesToScroll: 4,
      }); 
+    
+    $('.menu__btn').on('click', function(){
+       $('.menu__list').slideToggle();
+    });
+    $('.header__btn-menu').on('click', function(){
+      $('.header__box').toggleClass('active');
+   });
     var mixer = mixitup('.products__inner-box');
 });
